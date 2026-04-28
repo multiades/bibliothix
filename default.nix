63ac3abaade34df0081eabd8ba7a4d8f079bc1de
multiades <multiades@protonmail.com>
2026-04-28 17:20:31 +0300

    Added flake.nix, added LICENSE, updated README

--- default.nix
+++ default.nix
@@ -1,12 +1,9 @@
-/*
-No need for a flake, it is only a library of auxilliary functions.
-This file is the aggregator of the modules in ./lib.
-Assemble everything into a single lib attrset, self-referencing it instead of having the attribute set be a recursive one,  so modules can call each other.
-Because Nix is lazy, lib is only evaluated when its attributes are actually accessed. Each module receives the whole lib and can call siblings through it.
-*/
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 let
   lib = {
     list = builtins.import ./lib/list.nix lib;
     path = builtins.import ./lib/path.nix lib;
   };
-in lib
+in
+  lib
